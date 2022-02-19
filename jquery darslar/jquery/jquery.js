1640c8b1ff2972a2c0c92568ef287f27aeb752a2
Saidvalixon <99729716+Saidvalixon@users.noreply.github.com>
2022-02-19 11:55:12 +0500

    Add files via upload

---
+++ jquery darslar/jquery/jquery.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	$("li").click(function(){
+		$(this).hide(2000);
+	});
+});
+
+  $(document).ready(function(){
+	$("li").click(function(){
+		$(this).hide(2000);
+  });
+});
+
+//    $(document).ready(function(){
+// 	$("h1").click(function(){
+// 		$("p").show(2000);
+//     });
+// });
+
+//     $(document).ready(function(){
+// 	$("h1").click(function(){
+// 		$("p").show(2000);
+//     });
+// });
+
+//      $(document).ready(function(){
+// 	$("h1").click(function(){
+// 		$("p").show(2000);
+//     });
+// });
+
+//       $(document).ready(function(){
+// 	$("h1").click(function(){
+// 		$("p").show(2000);
+//     });
+// });
+
+//        $(document).ready(function(){
+// 	$("h1").click(function(){
+// 		$("p").show(2000);
+//     });
+// });
